f31d41fd93810536e60141b8a9549deada8adf2c
imbavirus <jvdwpsp@gmail.com>
2015-05-30 11:58:08 +0200

    c

--- src/server/scripts/Spells/spell_druid.cpp
+++ src/server/scripts/Spells/spell_druid.cpp
@@ -1109,20 +1109,28 @@ public:
 				{
 					caster->SetShapeshiftForm(FORM_AQUA);
 					caster->SetDisplayId(caster->GetModelForForm(FORM_AQUA));
-					caster->SetSpeed(MOVE_SWIM, caster->GetSpeed(MOVE_SWIM)*1.5f, false);
+					caster->SetSpeed(MOVE_SWIM, caster->GetSpeedRate(MOVE_SWIM)*1.5f, false);
+					caster->SetSpeed(MOVE_RUN, caster->GetSpeedRate(MOVE_RUN), false);
+					caster->SetSpeed(MOVE_FLIGHT, caster->GetSpeedRate(MOVE_FLIGHT), false);
+					caster->SetCanFly(false);
 				}
 				else if (caster->CanFlyInZone(caster->GetMapId(),caster->GetZoneId()))
 				{
 					caster->SetShapeshiftForm(FORM_FLIGHT);
 					caster->SetDisplayId(caster->GetModelForForm(FORM_FLIGHT));
 					caster->SetCanFly(true);
-					caster->SetSpeed(MOVE_FLIGHT, caster->GetSpeed(MOVE_FLIGHT)*2.5f, false);
+					caster->SetSpeed(MOVE_FLIGHT, caster->GetSpeedRate(MOVE_FLIGHT)*2.5f, false);
+					caster->SetSpeed(MOVE_RUN, caster->GetSpeedRate(MOVE_RUN), false);
+					caster->SetSpeed(MOVE_SWIM, caster->GetSpeedRate(MOVE_SWIM), false);
 				}
 				else 
 				{
 					caster->SetShapeshiftForm(FORM_TRAVEL);
 					caster->SetDisplayId(caster->GetModelForForm(FORM_TRAVEL));
-					caster->SetSpeed(MOVE_RUN, caster->GetSpeed(MOVE_RUN)*1.4f, false);
+					caster->SetSpeed(MOVE_RUN, caster->GetSpeedRate(MOVE_RUN)*1.4f, false);
+					caster->SetSpeed(MOVE_SWIM, caster->GetSpeedRate(MOVE_SWIM), false);
+					caster->SetSpeed(MOVE_FLIGHT, caster->GetSpeedRate(MOVE_FLIGHT), false);
+					caster->SetCanFly(false);
 				}
 			}
 		}
@@ -1132,6 +1140,11 @@ public:
 			if (Player* caster = GetCaster()->ToPlayer())
 			{
 				caster->SetShapeshiftForm(FORM_NONE);
+				caster->SetCanFly(false);
+				caster->SetDisplayId(caster->GetModelForForm(FORM_NONE));
+				caster->SetSpeed(MOVE_RUN, caster->GetSpeedRate(MOVE_RUN), false);
+				caster->SetSpeed(MOVE_SWIM, caster->GetSpeedRate(MOVE_SWIM), false);
+				caster->SetSpeed(MOVE_FLIGHT, caster->GetSpeedRate(MOVE_FLIGHT), false);
 			}
 		}
 
