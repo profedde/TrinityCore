aab4fdf113f9eff8533606eb5f51b5203ec68028
imbavirus <jvdwpsp@gmail.com>
2015-06-02 13:37:48 +0200

    Add checks for travel form

--- src/server/scripts/Spells/spell_druid.cpp
+++ src/server/scripts/Spells/spell_druid.cpp
@@ -79,7 +79,7 @@ public:
 			Unit* caster = GetCaster();
 			if (caster->HasAura(783) && !caster->IsFlying())
 				caster->RemoveAura(783);
-			caster->SetSpeed(MOVE_RUN, playerBaseMoveRate[MOVE_RUN] *2, false);
+			caster->SetSpeed(MOVE_RUN, playerBaseMoveRate[MOVE_RUN] *1.3, false);
 		}
 
 		void RemoveEffect(AuraEffect const* /*aurEff*/, AuraEffectHandleModes /*mode*/)
@@ -1178,7 +1178,7 @@ public:
 		{
 			if (Player* caster = GetCaster()->ToPlayer())
 			{ 
-				if (caster->IsInWater())
+				if (caster->IsInWater() && caster->HasAura(783))
 				{
 					caster->AddAura(1066,caster);
 				}
@@ -1186,14 +1186,14 @@ public:
 					((caster->GetMapId() == 530) || 
 					(caster->HasSpell(54197) && caster->GetMapId() == 571) ||
 					(caster->HasSpell(115913) && caster->GetMapId() == 870 && caster->GetZoneId() != 951 && caster->GetZoneId() != 929) ||
-					(caster->HasSpell(90267) && (caster->GetMapId() == 0 || caster->GetMapId() == 1 || caster->GetMapId() == 646))))
+					(caster->HasSpell(90267) && (caster->GetMapId() == 0 || caster->GetMapId() == 1 || caster->GetMapId() == 646))) && caster->HasAura(783))
 				{
-					if (caster->GetSkillValue(SKILL_RIDING) >= 300)
+					if (caster->GetSkillValue(SKILL_RIDING) >= 300 && caster->HasAura(783))
 						caster->AddAura(40120, caster);
 					else
 						caster->AddAura(33943, caster);
 				}
-				else 
+				else if (caster->HasAura(783))
 				{
 					caster->AddAura(165961, caster);
 				}
@@ -1254,15 +1254,17 @@ public:
 					(caster->HasSpell(115913) && caster->GetMapId() == 870 && caster->GetZoneId() != 951 && caster->GetZoneId() != 929) ||
 					(caster->HasSpell(90267) && (caster->GetMapId() == 0 || caster->GetMapId() == 1 || caster->GetMapId() == 646)))))
 				{
-					if (caster->GetSkillValue(SKILL_RIDING) >= 300)
+					if (caster->GetSkillValue(SKILL_RIDING) >= 300 && caster->HasAura(783))
 						caster->AddAura(40120, caster);
 					else
 						caster->AddAura(33943, caster);
 				}
-				else
+				else if (caster->HasAura(783))
 				{
 					caster->AddAura(165961, caster);
 				}
+				else
+					caster->DeMorph();
 			
 			}
 		}
@@ -1298,11 +1300,12 @@ public:
 		{
 			if (Player* caster = GetCaster()->ToPlayer())
 			{
-				if (caster->IsInWater())				
+				if (caster->IsInWater() && caster->HasAura(783))
 					caster->AddAura(1066, caster);				
-				else 
+				else if (caster->HasAura(783))
 					caster->AddAura(165961, caster);
-
+				else
+					caster->DeMorph();
 			}
 		}
 
@@ -1337,7 +1340,7 @@ public:
 		{
 			if (Player* caster = GetCaster()->ToPlayer())
 			{
-				if (caster->IsInWater())
+				if (caster->IsInWater() && caster->HasAura(783))
 				{
 					caster->AddAura(1066,caster);
 				}
@@ -1346,13 +1349,15 @@ public:
 					((caster->GetMapId() == 530) ||
 					(caster->HasSpell(54197) && caster->GetMapId() == 571) ||
 					(caster->HasSpell(115913) && caster->GetMapId() == 870 && caster->GetZoneId() != 951 && caster->GetZoneId() != 929) ||
-					(caster->HasSpell(90267) && (caster->GetMapId() == 0 || caster->GetMapId() == 1 || caster->GetMapId() == 646)))))
+					(caster->HasSpell(90267) && (caster->GetMapId() == 0 || caster->GetMapId() == 1 || caster->GetMapId() == 646)))) && caster->HasAura(783))
 				{
-					if (caster->GetSkillValue(SKILL_RIDING) >= 300)
+					if (caster->GetSkillValue(SKILL_RIDING) >= 300 && caster->HasAura(783))
 						caster->AddAura(40120, caster);
-					else
+					else if (caster->HasAura(783))
 						caster->AddAura(33943, caster);
 				}
+				else
+					caster->DeMorph();
 			}
 		}
 
