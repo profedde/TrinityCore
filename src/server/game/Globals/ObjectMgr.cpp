e82adea6259ed3d1596a6e2cbf809470c13e14c5
imbavirus <jvdwpsp@gmail.com>
2015-06-04 10:21:28 +0200

    b

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -1145,8 +1145,7 @@ EquipmentInfo const* ObjectMgr::GetEquipmentInfo(uint32 entry, int32& id)
     EquipmentInfoContainer::const_iterator itr = _equipmentInfoStore.find(entry);
 	if (itr == _equipmentInfoStore.end())
 	{
-		uint32 abc = itr->first;
-		TC_LOG_INFO("server.loading", ">> ERROR 1 << >> %u << >> %u << >> %u <<", entry, id, abc); 
+		TC_LOG_INFO("server.loading", ">> ERROR 1 << >> %u << >> %u <<", entry, id); 
 		return NULL;
 	}
 	if (itr->second.empty())
@@ -1163,8 +1162,7 @@ EquipmentInfo const* ObjectMgr::GetEquipmentInfo(uint32 entry, int32& id)
     }
     else
     {
-		uint32 abc = itr->first;
-		TC_LOG_INFO("server.loading", ">> ERROR 4 << >> %u << >> %u << >> %u <<", entry, id, abc);
+		TC_LOG_INFO("server.loading", ">> ERROR 4 << >> %u << >> %u <<", entry, id);
 		EquipmentInfoContainerInternal::const_iterator itr2 = itr->second.find(id);
         if (itr2 != itr->second.end())
             return &itr2->second;
