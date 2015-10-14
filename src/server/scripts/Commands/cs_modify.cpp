07faa649da80e6fbb58b6e5b483842c6ff0b7b5b
imbavirus <jvdwpsp@gmail.com>
2015-10-14 23:05:49 +0200

    fix display of mod power

--- src/server/scripts/Commands/cs_modify.cpp
+++ src/server/scripts/Commands/cs_modify.cpp
@@ -607,7 +607,7 @@ public:
 		}
 
 		target->SetPower(value,value1);
-		handler->PSendSysMessage("Power value has been set to: ", value1);
+		handler->PSendSysMessage("Power value has been set to: ", uint32(value1));
 
 
 	}
@@ -692,7 +692,7 @@ public:
 		}
 		
 		uint32 value1 = target->GetPower(value);
-		handler->PSendSysMessage("Power value is: ", value1);
+		handler->PSendSysMessage("Power value is: ", uint32(value1));
 	}
     //Edit Player Speed
     static bool HandleModifySpeedCommand(ChatHandler* handler, const char* args)
